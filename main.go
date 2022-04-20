3bf64b4a5aa294ea966e0f6b8ef89fad16834d64
saiashish9 <saiashish3760@gmail.com>
2022-04-20 17:14:30 +0530

    updated functions for uploading binary to s3 and kafka server

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+
+	appKafka "./kafka"
+)
 
 func main() {
 	fmt.Print("go kafka")
+	go appKafka.StartKafka()
+	fmt.Println("kafka has been started")
+	time.Sleep(10 * time.Minute)
 }
